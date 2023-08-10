d9611f3f2625bb406df0ecca4921a289560b1fa4
sarakhman <artur.sarahman@gmail.com>
2023-08-10 23:14:34 +0300

    Add initial configuration

---
+++ src/main/java/mate/academy/model/Role.java
@@ -0,0 +1,46 @@
+package mate.academy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private RoleName roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" + "id=" + id + ", roleName=" + roleName + '}';
+    }
+
+    public enum RoleName {
+        ADMIN, USER
+    }
+}
